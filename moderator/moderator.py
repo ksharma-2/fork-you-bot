1bda0e08c46d29d6868e4e5226ae77ff175c3216
marcos_gonzalezmayedo <magonzal@ualberta.ca>
2024-01-06 16:31:39 -0700

    Added moderator test

--- moderator/moderator.py
+++ moderator/moderator.py
@@ -1,7 +1,24 @@
-import requests 
+#IMPORTS:
+import requests as req
 
-moderatorAPIurl = "https://api.openai.com/v1/moderations/"
+print('Ran moderator')
 
-def evaluate(text) {
-	r = req
-}
+def evaluate(message):
+    """
+        Reads in message object and passess it into Moderation Model for inference.
+    """
+    
+    #Call stringify method of object to send to Moderation Model.
+    # message = message.stringify()
+
+    headers = {"Content-Type": "application/json","Authorization" : "Bearer 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"}
+
+    data = {"input": "keep yourself safe"} #Sample test message
+
+    r = req.post('https://api.openai.com/v1/moderations', headers=headers, data=data)
+    # r = req.post('https://httpbin.org/post', data={'key': 'value'})
+
+    print(r.json())
+
+if __name__ == "__main__":
+    evaluate("test")
