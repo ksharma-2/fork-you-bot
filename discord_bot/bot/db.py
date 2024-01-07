068f021c51cd1dd1546f20de1d5f8ccbdca02b5b
Gurbir Sandha <77547332+gurbirsandha@users.noreply.github.com>
2024-01-07 00:04:21 -0700

    setup mongo and working locally to store user score by author and server

--- discord_bot/bot/db.py
+++ discord_bot/bot/db.py
@@ -10,64 +10,55 @@ class MongoDbHandler:
     def __init__(self, connection_string: str, password: str):
         self.conn_str = connection_string.replace('<password>', password)
         self.client = pymongo.MongoClient(self.conn_str)
-        self.db = self.client["db"]
+        self.db = self.client["fork-you"]
 
-    def updateUserScore(self, message: UserMessage) -> bool:
+    def updateUserScore(self, message_author, message_server) -> bool:
         try:
             existsUser = self.db.users.find_one({
-                "author": message.author,
-                "server": message.server
+                "author": message_author,
+                "server": message_server
             })
             if existsUser:
-                if "tags_count" not in existsUser:
-                    existsUser["tags_count"] = {}
-                tcd = existsUser["tags_count"]
-                for tag, count in message.tags_count.items():
-                    if tag in tcd:
-                        tcd[tag] += count
-                    else:
-                        tcd[tag] = count
                 self.db.users.update_one({
                     "author": existsUser['author'],
                     "server": existsUser['server']
                 }, {
-                    "$set": {"score": existsUser['score'] + message.score, "tags_count": tcd}
+                    "$set": {"score": existsUser['score'] + 1}
                 })
             else:
                 self.db.users.insert_one({
-                    "author": message.author,
-                    "server": message.server,
-                    "score": message.score,
-                    "tags_count": message.tags_count
+                    "author": message_author,
+                    "server": message_server,
+                    "score": 1,
                 })
             return True
         except Exception as e:
             print(e)
             return False
 
-    def getUserDetails(self, server_id, user_id) -> Union[dict, None]:
-        try:
-            return self.db.users.find_one({
-                "server": server_id,
-                "author": user_id
-            })
-        except Exception as e:
-            print(e)
-            return None
+    # def getUserDetails(self, server_id, user_id) -> Union[dict, None]:
+    #     try:
+    #         return self.db.users.find_one({
+    #             "server": server_id,
+    #             "author": user_id
+    #         })
+    #     except Exception as e:
+    #         print(e)
+    #         return None
 
-    def getTopOffensive(self, client: discord.Client, guild_id: int) -> list:
-        guild = client.get_guild(guild_id)
-        res = []
-        for member in guild.members:
-            data = self.getUserDetails(guild_id, member.id)
-            if not data : 
-                continue
-            df = {
-                    "name": member.display_name,
-                    "score": data["score"],
-                    "tags_count": data["tags_count"],
-                }
-            df["image_url"] = member.display_avatar.url
-            res.append(df)
-        res = sorted(res, key=lambda x: -x["score"])
-        return res
+    # def getTopOffensive(self, client: discord.Client, guild_id: int) -> list:
+    #     guild = client.get_guild(guild_id)
+    #     res = []
+    #     for member in guild.members:
+    #         data = self.getUserDetails(guild_id, member.id)
+    #         if not data : 
+    #             continue
+    #         df = {
+    #                 "name": member.display_name,
+    #                 "score": data["score"],
+    #                 "tags_count": data["tags_count"],
+    #             }
+    #         df["image_url"] = member.display_avatar.url
+    #         res.append(df)
+    #     res = sorted(res, key=lambda x: -x["score"])
+    #     return res
--- discord_bot/bot/main.py
+++ discord_bot/bot/main.py
@@ -10,6 +10,7 @@ from flask import request
 from flask_cors import CORS
 from utils import get_message_details
 from moderator.moderator import evaluate
+from db import MongoDbHandler
 
 load_dotenv()
 
@@ -26,9 +27,16 @@ CORS(app)
 @client.event
 async def on_message(message):
     message_content = message.content
-    message_author = message.author
+    message_author_id = message.author.id
+    message_server = message.guild.id
+    
     if evaluate(message_content):
         await message.delete()
+        is_successful = DB.updateUserScore(message_author_id, message_server)
+        if is_successful:
+            print("Updated user info")
+        else:
+            print("User update failed")
 
 @client.event
 async def on_ready():
@@ -39,7 +47,7 @@ def start_server():
     app.run(debug=False)
 
 if __name__ == "__main__":
-    # DB = MongoDbHandler(os.environ["DB_URL"], os.environ["DB_PASSWORD"])
+    DB = MongoDbHandler(os.environ["DB_URL"], os.environ["DB_PASSWORD"])
     thread = threading.Thread(target=start_server, args=[])
     thread.start()
     client.run(os.environ["DISCORD_CLIENT_SECRET"])
